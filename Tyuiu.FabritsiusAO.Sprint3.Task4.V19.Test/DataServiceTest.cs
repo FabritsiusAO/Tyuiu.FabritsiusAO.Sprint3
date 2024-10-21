cba4e4012839680dc9779da484baa398ac6c9ac7
Андрей Фабрициус <Tyuiu.FabritsiusAO@outlook.com>
2024-10-21 22:56:32 +0500

    Task4

---
+++ Tyuiu.FabritsiusAO.Sprint3.Task4.V19.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.FabritsiusAO.Sprint3.Task4.V19.Lib;
+
+namespace Tyuiu.FabritsiusAO.Sprint3.Task4.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = 5;
+            int stopValue = 5;
+            double wait = 1.446;
+            double res = ds.Calculate(startValue, stopValue);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.FabritsiusAO.Sprint3.Task4.V19/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.FabritsiusAO.Sprint3.Task4.V19.Lib;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        int startValue = -5;
+        int stopValue = 5;
+
+        Console.Title = "Спринт #3 | Выполнил: Фабрициус А. О. | ИСТНб-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #19                                                             *");
+        Console.WriteLine("* Выполнил: Фабрициус Андрей Олегович | ИСТНб-24-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить              *");
+        Console.WriteLine("* значение функции y=x/(cos(x) + x)+0.5 При х = 0 прервать цикл.          *");
+        Console.WriteLine("* Полученные значения перемножать.                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Начальное значение: " + startValue);
+        Console.WriteLine("Конечное значение: " + stopValue);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.Calculate(startValue, stopValue));
+    }
+}
