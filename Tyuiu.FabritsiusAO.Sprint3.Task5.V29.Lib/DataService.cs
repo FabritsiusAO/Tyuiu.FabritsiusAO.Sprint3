dea4f5b5f668002cc3546c38489da78759ae567e
Андрей Фабрициус <Tyuiu.FabritsiusAO@outlook.com>
2024-10-27 21:38:32 +0500

    Task5

--- Tyuiu.FabritsiusAO.Sprint3.Task5.V29.Lib/DataService.cs
+++ Tyuiu.FabritsiusAO.Sprint3.Task5.V29.Lib/DataService.cs
@@ -6,10 +6,10 @@ namespace Tyuiu.FabritsiusAO.Sprint3.Task5.V29.Lib
     {
         public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
         {
-            double res = 8.574;
-            for (int i = startValue1; i <= stopValue1; i++)
+            double res = 0;
+            for (double i = startValue1; i <= stopValue1; i++)
             {
-                for (int k = startValue2; k <= stopValue2; k++)
+                for (double k = startValue2; k <= stopValue2; k++)
                 {
                     double y = x / k + Math.Sin(x); res += y;
                 }
